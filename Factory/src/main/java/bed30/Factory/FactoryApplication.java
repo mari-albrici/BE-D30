a7edcc0d5c5940720a4d43357f0f2a39af28f436
Marianna <121226166+mari-albrici@users.noreply.github.com>
2023-06-16 13:54:29 +0200

    FACTORY CREATED

--- Factory/src/main/java/bed30/Factory/FactoryApplication.java
+++ Factory/src/main/java/bed30/Factory/FactoryApplication.java
@@ -1,5 +1,7 @@
 package bed30.Factory;
 
+import bed30.Factory.entities.ProcessControl;
+import bed30.Factory.entities.ProcessControlFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FactoryApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FactoryApplication.class, args);
+
+		ProcessControl probe = ProcessControlFactory.createProbe( 1, 123.456, 789.012, 6, "http://host/alarm");
+
+		probe.sendAlarm();
 	}
 
 }
