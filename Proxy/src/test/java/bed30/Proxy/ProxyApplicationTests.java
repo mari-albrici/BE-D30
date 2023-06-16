35edc9c1f87a592b4af19aeaf04a974cbd112a1f
Marianna <121226166+mari-albrici@users.noreply.github.com>
2023-06-16 09:16:40 +0200

    Added Initializr files

---
+++ Proxy/src/test/java/bed30/Proxy/ProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package bed30.Proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
